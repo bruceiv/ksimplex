296364b2512c821198f31858a19e7ec867f1656d
Aaron Moss <a3moss@uwaterloo.ca>
2013-05-29 17:54:54 -0400

    Filled out set of host-side ops on device-side kilomp

--- kilomp/kilomp_cuda.cuh
+++ kilomp/kilomp_cuda.cuh
@@ -83,5 +83,92 @@ __host__ void copy_hd(mpv d_d, mpv s, u32 n, u32 a_l) {
 	}
 }
 
+/**
+ * Copies a device mp-vector into another device mp-vector.
+ * @param d_d       The destination vector
+ * @param s_d		The source vector
+ * @param n			The number of elements in the source vector (the destination vector should be 
+ *                  allocated for at least as many)
+ * @param a_l		The number of data limbs allocated in the source vector (the destination vector 
+ * 					should be allocated for at least as many)
+ */
+__host__ void copy_dd(mpv d_d, mpv s_d, u32 n, u32 a_l) {
+	// Copy the device pointers for the limb arrays down
+	limb*[1+a_l] d_h;
+	limb*[1+a_l] s_h;
+	cudaMemcpy(d_h, d_d, (1+a_l)*sizeof(limb*), cudaMemcpyDeviceToHost); CHECK_CUDA_SAFE
+	cudaMemcpy(s_h, s_d, (1+a_l)*sizeof(limb*), cudaMemcpyDeviceToHost); CHECK_CUDA_SAFE
+	
+	// Copy limb arrays across on device
+	for (u32 i = 0; i <= a_l; ++i) {
+		cudaMemcpy(d_h[i], s_h[i], n*limb_size, cudaMemcpyDeviceToDevice); CHECK_CUDA_SAFE
+	}
+}
+
+/**
+ * Copies a device mp-vector into a host mp-vector.
+ * @param d	        The destination vector
+ * @param s_d		The source vector
+ * @param n			The number of elements in the source vector (the destination vector should be 
+ *                  allocated for at least as many)
+ * @param a_l		The number of data limbs allocated in the source vector (the destination vector 
+ * 					should be allocated for at least as many)
+ */
+__host__ void copy_dh(mpv d, mpv s_d, u32 n, u32 a_l) {
+	// Copy the device pointers for the limb arrays down
+	limb*[1+a_l] s_h;
+	cudaMemcpy(s_h, s_d, (1+a_l)*sizeof(limb*), cudaMemcpyDeviceToHost); CHECK_CUDA_SAFE
+	
+	// Copy limb arrays across on device
+	for (u32 i = 0; i <= a_l; ++i) {
+		cudaMemcpy(d[i], s_h[i], n*limb_size, cudaMemcpyDeviceToHost); CHECK_CUDA_SAFE
+	}
+}
+
+/**
+ * Expands a mp-vector on device
+ * @param v_d		The vector
+ * @param n			The number of elements in the vector
+ * @param old_l		The number of limbs already allocated
+ * @param alloc_l	The new number of limbs (must be greater than old_l)
+ * @return the modified vector
+ */
+__host__ mpv expand_d(mpv v_d, u32 n, u32 old_l, u32 alloc_l) {
+	// Allocate new mpv on device
+	mpv w_d;
+	cudaMalloc((void**)&w_d, (1+alloc_l)*sizeof(limb*)); CHECK_CUDA_SAFE 
+	
+	// Copy old limb pointers
+	cudaMemcpy(w_d, v_d, (1+old_l)*sizeof(limb*), cudaMemcpyDeviceToDevice); CHECK_CUDA_SAFE
+	
+	// Allocate new data limbs
+	limb*[alloc_l-old_l] w_h;
+	for (u32 i = 0; i < alloc_l-old_l; ++i) {
+		cudaMalloc((void**)&w_h[i], n*limb_size); CHECK_CUDA_SAFE
+	}
+	
+	// Copy new limb pointers to device
+	cudaMemcpy(w_d+old_l+1, w_h, (alloc_l-old_l)*sizeof(limb*), cudaMemcpyHostToDevice); CHECK_CUDA_SAFE
+	
+	//replace v with w
+	cudaFree(v_d);
+	return w_d;
+}
+
+/**
+ * Frees a mp-vector on device
+ * @param v_d		The vector
+ * @param alloc_l	The number of limbs allocated
+ */
+__host__ void clear_d(mpv v_d, u32 alloc_l) {
+	// Copy the device pointers for the limb arrays down
+	limb*[1+alloc_l] v_h;
+	cudaMemcpy(v_h, v_d, (1+alloc_l)*sizeof(limb*), cudaMemcpyDeviceToHost); CHECK_CUDA_SAFE
+	
+	// Free device arrays
+	for (u32 i = 0; i <= alloc_l; ++i) { cudaFree(v_h[i]); }
+	cudaFree(v_d);
+}
+
 } /* namespace kilo */
 
