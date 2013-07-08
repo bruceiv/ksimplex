318dbf113680e7d5155e7b2227faa2feea8cb70c
Aaron Moss <a3moss@uwaterloo.ca>
2013-07-08 17:20:24 -0400

    LRSPP now normalizes each output row by its least common denominator

--- lrspp.cpp
+++ lrspp.cpp
@@ -22,6 +22,33 @@
 
 using namespace ksimplex;
 
+/** 
+ * Reduces the rows in an LRS instance to lowest terms.
+ */
+void toLowestTerms(lrs::lrs& l, u32 n, u32 d) {
+	// Initialize GCD to determinant
+	mpz_t g;
+	mpz_init(g);
+	
+	// Calculate GCD
+	for (u32 i = 0; i <= n; ++i) {
+		mpz_set(g, l.elem(i, 0));
+		
+		// while GCD is != 1
+		for (u32 j = 1; j <= d && mpz_cmp_ui(g, 1) != 0; ++j) {
+			mpz_gcd(g, g, l.elem(i, j));      // g = gcd(g, l[i,j])
+		}
+		
+		if ( mpz_cmp_ui(g, 1) > 0 ) {  // gcd is > 1
+			for (u32 j = 0; j <= d; ++j) {
+				mpz_divexact(l.elem(i, j), l.elem(i, j), g);
+			}
+		}
+	}
+	
+	mpz_clear(g);
+}
+
 /**
  * Finds an initial basis for a LP, and outputs in a format readable by KSimplex.
  * 
@@ -51,8 +78,11 @@ int main(int argc, char** argv) {
 	l.setLPObj(obj);
 	l.getFirstBasis(false);
 	
+	// Reduce tableau to lowest terms
+	toLowestTerms(l, n, d);
+	
 	// Write size and dimension of problem, as well as determinant
-	std::cout << n << " " << d << " " << hex(l.getDeterminant()) << std::endl;
+	std::cout << n << " " << d << " " << 1 /*hex(l.getDeterminant())*/ << std::endl;
 	
 	// Write initial basis
 	lrs::ind* bas = l.getLPBasis();
